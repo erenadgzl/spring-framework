578e64d60fc1760ffc214ae7dbccf2281861c767
Eren Adıgüzel <erenadiguzellll@gmail.com>
2020-09-01 16:33:49 +0300

    test

---
+++ spring-websocket/src/main/java/com/sprinprojects/Application.java
@@ -0,0 +1,15 @@
+package com.sprinprojects;
+
+/* 
+Eren created on 31.08.2020 
+*/
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ spring-websocket/src/main/java/com/sprinprojects/config/WebSocketConfig.java
@@ -0,0 +1,8 @@
+package com.sprinprojects.config;
+
+/* 
+Eren created on 31.08.2020 
+*/
+
+public class WebSocketConfig {
+}
